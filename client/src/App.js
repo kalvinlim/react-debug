b45fa4cbe04ddb344854601b4191cb8bb7a816db
Yong Lin <yonglin1618@gmail.com>
2017-12-17 01:50:06 -0600

    Set initial state value

--- client/src/App.js
+++ client/src/App.js
@@ -3,28 +3,34 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: {
+        temperature: {
+          value: 0
+        }
+      }
+    };
+  }
 
-  state = {data: []}
-  componentDidMount(){
+  componentDidMount() {
     fetch('/sensor/')
       .then(res => res.json())
-      .then(data => this.setState({data}));
-      this.state.foo = "hi";
+      .then(data => this.setState({ data }));
   }
   render() {
-    console.log(this.state.data.temperature.value);
-    console.log(this.state.foo);
-
+    const temperatureValue = this.state.data.temperature.value;
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
-          
-          <div>Result: {this.state.foo} 
-          
+
+          <div>
+            Result: {temperatureValue}
           </div>
-         
+
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
